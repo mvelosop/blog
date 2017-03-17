e1201a7204df250c2add61667dc4bf1c46b1d105
Miguel Veloso <mvelosop@gmail.com>
2017-03-17 23:26:14 +0000

    Lista implementación definitiva del Clipboard.js

--- static/scripts/snippets.js
+++ static/scripts/snippets.js
@@ -1,9 +1,3 @@
-// var snippets = $('.snippet');
-
-// for (snippet of snippets) { 
-// 	snippet.firstChild.insertAdjacentHTML('beforebegin', '<button class="btn" data-clipboard-snippet><img class="clippy" width="13" src="/images/clippy.svg" alt="Copiar al clipboard"></button>');
-//  }; 
- 
  var clipboardSnippets = new Clipboard('[data-clipboard-snippet]', { target: function (trigger) { return trigger.nextElementSibling; } });
 
  clipboardSnippets.on('success', function (e) { e.clearSelection(); showTooltip(e.trigger, 'Copiado!'); }); 
